1e4e1d5be8d94bed7c2deb74690249c63d8dd35c
Bram <bram.schumacher@hotmail.com>
2026-01-27 12:00:28 +0100

    Move to production database

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -3,7 +3,8 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_SELF_HOSTED_URL!);
+
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
 	return <ConvexProvider client={convex}>{children}</ConvexProvider>;
