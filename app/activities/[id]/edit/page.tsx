413f24afd88fd5017f65156bc4fe709d6dd20d5f
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2026-03-28 16:47:26 +0100

    fix static site generation

--- app/activities/[id]/edit/page.tsx
+++ app/activities/[id]/edit/page.tsx
@@ -1,7 +1,8 @@
 import EditActivityPage from "./edit-page-client";
 
 export function generateStaticParams() {
-  return [];
+  // See comment in app/activities/[id]/page.tsx for why a placeholder is used.
+  return [{ id: "_" }];
 }
 
 export default function Page() {
